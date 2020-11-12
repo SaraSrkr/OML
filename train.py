ac30ccd0639257f092af59b55510d7cff87e7edc
Sara Ahmed <73200360+SaraSrkr@users.noreply.github.com>
2020-11-12 03:55:40 +0300

    Update train.py

--- train.py
+++ train.py
@@ -9,6 +9,15 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+   
+    
+ run = Run.get_context()
+# TODO: Create TabularDataset using TabularDatasetFactory
+    # Data is located at:
+    # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    
+ds =TabularDatasetFactory.from_delimited_files(['https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'])
+
 
 def clean_data(data):
     # Dict for cleaning data
@@ -37,7 +46,11 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     return x_df, y_df
 
+     x, y = clean_data(ds)
 
+    # TODO: Split data into train and test sets.
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
 def main():
     # Add arguments to script
@@ -48,21 +61,12 @@ def main():
 
     args = parser.parse_args()
 
-    run = Run.get_context()
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
     
-    # TODO: Create TabularDataset using TabularDatasetFactory
-    # Data is located at:
-    # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-    ds =TabularDatasetFactory.from_delimited_files(['https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'])
-
-    x, y = clean_data(ds)
-
-    # TODO: Split data into train and test sets.
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+    
+   
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
